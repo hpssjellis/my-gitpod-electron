8bc1607180c736662819aae18c07c69076789513
Jeremy Ellis <keyfreemusic@gmail.com>
2019-06-17 10:46:42 -0700

    Update a01-electron-create.sh

--- a01-electron-create.sh
+++ a01-electron-create.sh
@@ -2,4 +2,11 @@
 
 
 
-#nothing here
+# Clone the repository
+git clone https://github.com/electron/electron-quick-start
+# Go into the repository
+cd electron-quick-start
+# Install dependencies
+npm install
+# Run the app
+npm start
